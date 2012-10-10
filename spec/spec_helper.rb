d8901d823b299a89000f951aeed185d9be7df859
Javier Mey <javier.mey@sage.com>
2012-10-10 13:51:46 +0200

    Finished test for yaml_write

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,11 @@
 require 'rubygems'
-require 'bundler/setup'
 
-
-require "transdifflation/version"
-require "transdifflation/yaml_reader"
-require "transdifflation/yaml_writer"
-require "transdifflation/exceptions"
-require "transdifflation/utilities"
+require 'transdifflation/version'
+require 'transdifflation/yaml_reader'
+require 'transdifflation/yaml_writer'
+require 'transdifflation/exceptions'
+require 'transdifflation/utilities'
+require 'yaml'
 
 
 RSpec.configure do |config|
--- spec/yaml_reader_spec.rb
+++ spec/yaml_reader_spec.rb
@@ -49,4 +49,39 @@ describe :YAMLReader do
       expect {Transdifflation::YAMLReader.read_YAML_from_gem(a_gem, a_path)}.to_not raise_error(ArgumentError)
     end
   end
+
+  describe :read_YAML_from_pathfile do
+    it 'should raise an error exception when a path does not exist' do
+      File.stub(:file?).and_return(false)
+      a_path = './'
+
+      expect {Transdifflation::YAMLReader.read_YAML_from_pathfile(a_path)}.to raise_error(ArgumentError)
+    end
+
+    it 'should not raise an error exception when a path exists (rspec)' do
+      File.stub(:basename).and_return(nil)
+      File.stub(:file?).and_return(true)
+      a_path = 'README.md'
+
+      Transdifflation::YAMLReader.stub(:get_YAML_content_from_YAML_file).and_return(':a => 2')
+      expect {Transdifflation::YAMLReader.read_YAML_from_pathfile(a_path)}.to_not raise_error(ArgumentError)
+    end
+  end
+
+  describe :get_YAML_content_from_YAML_file do
+    it 'should return {} when there is no YAML file' do
+      a_yml_path = 'bad.yml'
+      YAML.stub(:load_file).and_return(false)
+      
+     Transdifflation::YAMLReader.get_YAML_content_from_YAML_file(a_yml_path).should == {}
+    end
+
+    it 'should return the YAML in case it load properly the file' do
+      a_yml_path = 'good.yml'
+      YAML.stub(:load_file).and_return(['badger', 'elephant', 'tiger'])
+
+      Transdifflation::YAMLReader.get_YAML_content_from_YAML_file(a_yml_path).should == ['badger', 'elephant', 'tiger']
+    end
+  end
+
 end
